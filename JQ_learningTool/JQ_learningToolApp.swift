3371de1af52f8c07ec9d3236d724e8d37db076d6
lvjiaqi <405164649@qq.com>
2024-10-15 23:09:54 +0800

    Initial Commit

---
+++ JQ_learningTool/JQ_learningToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  JQ_learningToolApp.swift
+//  JQ_learningTool
+//
+//  Created by lvjiaqi on 10/15/24.
+//
+
+import SwiftUI
+
+@main
+struct JQ_learningToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
